fab382cb13b1c71ee8c942a935e9d083a8c2fe9b
Koscial <jan.koscialkowski@gmail.com>
2019-05-08 04:15:03 +0200

    Closes #6: refined paths in all modules, started app.py, finished downloader.py, reader class almost ready, only to add PyTorch conversion.

--- amphibian/fetch/downloader.py
+++ amphibian/fetch/downloader.py
@@ -3,39 +3,65 @@ import pandas_datareader as pd_datareader
 import os
 import argparse
 
-DATA_DIR = '../../data/'
-ALL_VALUES_DIR = '../../data/all_values/'
+from pathlib import Path
+
+DATA_DIR = str(Path(os.path.abspath(__file__)).parents[2]) + '/data/'
+ALL_VALUES_DIR = DATA_DIR + 'all_values/'
 
 
-def download(quote_type: str, exchange: str, business: str):
+def download(quote_type: str, region: str, business: str = None):
+    """Function for quote downloading.
+
+    :param quote_type:
+    :param region:
+    :param business:
+    :return:
+    """
+    # Create necessary high-level paths
     if not os.path.exists(ALL_VALUES_DIR):
         os.mkdir(ALL_VALUES_DIR)
     if not os.path.exists(ALL_VALUES_DIR + quote_type):
         os.mkdir(ALL_VALUES_DIR + quote_type)
-    if not os.path.exists(ALL_VALUES_DIR + quote_type + '/' + exchange):
-        os.mkdir(ALL_VALUES_DIR + quote_type + '/' + exchange)
+
+    # Read selected tickers and regions data
     tickers = pd.read_csv(DATA_DIR + 'tickers_' + quote_type + '.csv')
-    # Creating logical vector depending on selected exchange & business
-    tickers_logical = tickers['Exchange'] == exchange
+    regions = pd.read_csv(DATA_DIR + 'regions.csv')
+
+    # Consider business for stocks or not
+    tickers_logical = tickers['Exchange'].isin(
+        regions.loc[regions.region == region, 'Exchange'].values
+    )
     if quote_type == 'stocks' and business:
+        quotes_path = ALL_VALUES_DIR + quote_type + '/' + business + '/' + region
         tickers_logical = (
             tickers_logical & (tickers['Category Name'] == business)
         )
+    else:
+        quotes_path = ALL_VALUES_DIR + quote_type + '/' + region
+
+    # Create directory for storing selected quotes
+    if not os.path.exists(quotes_path):
+        os.makedirs(quotes_path)
+
+    # Attempt to download data
     for ticker in tickers.loc[tickers_logical, 'Ticker']:
         print(ticker)
         try:
-            pd_datareader.DataReader(ticker, 'yahoo').to_csv(ALL_VALUES_DIR + quote_type + '/' + exchange + '/' + ticker)
+            pd_datareader.DataReader(ticker, 'yahoo').to_csv(
+                quotes_path + '/' + ticker
+            )
         except:
             pass
 
 
 if __name__ == '__main__':
+    # Parse input arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('-T', '--Type',
                         help='Export values of selected type')
-    parser.add_argument('-E', '--Exchange',
-                        help='Export values from selected exchange')
+    parser.add_argument('-R', '--Region',
+                        help='Export values from selected region')
     parser.add_argument('-B', '--Business',
                         help='Export values for selected business')
     args = parser.parse_args()
-    download(args.Type, args.Exchange, args.Business)
+    download(args.Type, args.Region, args.Business)
--- amphibian/fetch/reader.py
+++ amphibian/fetch/reader.py
@@ -1,3 +1,101 @@
-'''
-test
-'''
+import numpy as np
+import os
+import pandas as pd
+import datetime
+
+from pathlib import Path
+
+DATA_DIR = str(Path(os.path.abspath(__file__)).parents[2]) + '/data/'
+ALL_VALUES_DIR = DATA_DIR + 'all_values/'
+
+
+class AmphibianReader:
+    def __init__(self, data_path: str,
+                 min_date: datetime.datetime,
+                 max_date: datetime.datetime):
+        """Class constructor.
+
+        :param data_path:
+        :param min_date:
+        :param max_date:
+        """
+        self.data_path = data_path
+        self.min_date = min_date
+        self.max_date = max_date
+        # Read regions data
+        regions = pd.read_csv(DATA_DIR + 'regions.csv')
+        self.regions = regions.region.unique()
+
+    def read_csvs(self):
+        """
+
+        :return:
+        """
+        self.csvs = {}
+        self.quotes = {}
+        # Iterate over regions
+        for reg in self.regions:
+            self.csvs[reg] = []
+            self.quotes[reg] = []
+            # Iterate over files for this region
+            for file in os.listdir(self.data_path + '/' + reg):
+                df = pd.read_csv(self.data_path + '/' + reg + '/' + file)
+                df = df.assign(
+                    Date=pd.to_datetime(df.Date, format='%Y-%m-%d')
+                ).drop_duplicates(subset='Date')
+                # Gather conditions for min and max dates
+                # Initial date for the quote at or before the minimum date?
+                min_date_cond = df.Date.iloc[0] <= self.min_date
+                # Last date for the quote at or after the maximum date?
+                max_date_cond = df.Date.tail(1).iloc[0] >= self.max_date
+                if min_date_cond and max_date_cond:
+                    self.csvs[reg].append(df)
+                    self.quotes[reg].append(file)
+        return self.csvs
+
+    def get_unique_dates(self):
+        """Extract unique dates from all csvs.
+
+        These are necessary, because trade does not occur on all days in all
+        countries and the data size has to be unified somehow.
+
+        :return:
+        """
+        # Initialize an empty set
+        unique_dates = set()
+        # Iterate over regions
+        for reg in self.regions:
+            # Iterate over files for this region
+            for file in self.csvs[reg]:
+                unique_dates = unique_dates.union(set(file.Date))
+            self.unique_dates = pd.DataFrame(
+                {'Date':list(unique_dates)}
+            ).sort_values('Date')
+        return self.unique_dates
+
+    def create_numpy(self):
+        """Create 3-dim (date x prices x company) numpy arrays for each region.
+
+        Data is left joined to unique dates so that the date range is the same
+        for all companies, possibly filled with NaN's.
+
+        :return:
+        """
+        self.numpy = {}
+        # Iterate over regions
+        for reg in self.regions:
+            array_list = []
+            # Iterate over files for this region
+            for df in self.csvs[reg]:
+                # Left join the current dataframe to the unique date dataframe
+                joined_df = self.unique_dates.set_index('Date').join(
+                    df.set_index('Date'),
+                    how='left'
+                ).reset_index().drop('Date', axis=1)
+                array = np.expand_dims(joined_df.values, 2)
+                array_list.append(array)
+            self.numpy[reg] = np.concatenate(array_list, axis=2)
+        return self.numpy
+
+    #def create_torch(self):
+
