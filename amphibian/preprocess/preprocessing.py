e977e28277c4fc7b25ccc542d0ac60d2e48abce7
michal_jaworski <michal.jaworski@students.mimuw.edu.pl>
2019-05-15 15:40:39 +0200

    fill_nan, scaling added

--- amphibian/preprocess/preprocessing.py
+++ amphibian/preprocess/preprocessing.py
@@ -1,36 +1,65 @@
+import torch
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
 from torchvision import transforms
 
 class TimeSeriesDataset(Dataset):
-    def __init__(self, amReader, int_len = 30, input_reg = 'ASIA_PACIFIC', pred_reg = 'EMEIA', transform=None):
+    def __init__(self, amReader, int_len = 5, input_reg = 'ASIA_PACIFIC', pred_reg = 'EMEIA', transform=None):
         self.transform = transform
         self.amReader = amReader
+        self.whole_set = {'observations': self.amReader.torch[input_reg],
+                          'y': self.amReader.torch[pred_reg][:, 5, 0]}
+        if self.transform:
+            self.whole_set = self.transform(self.whole_set)
+
         self.len = self.amReader.torch[input_reg].size()[0] - int_len
-        self.observation = {}
+        self.observations = {}
         self.y = {}
         for i in range(self.len):
-            self.observation[i] = self.amReader.torch[input_reg][i:i + int_len, :, :]
-            self.y[i] = self.amReader.torch[pred_reg][i + int_len - 1, 5, 0] # we want to predict Adj Close price
+            self.observations[i] = self.whole_set['observations'][i:i + int_len, :, :]
+            self.y[i] = self.whole_set['y'][i + int_len - 1] # we want to predict Adj Close price
 
     def __len__(self): # return the length of the data
         return self.len
 
     def __getitem__(self, item): # return one item on the index
-        obs = self.observation[item]
+        obs = self.observations[item]
         y = self.y[item]
-        sample = {'Observations': obs, 'y': y}
-        if self.transform:
-            sample = self.transform(sample)
+        sample = {'observations': obs, 'y': y}
 
         return sample
 
 class Fill_NaN(object):
+    def __call__(self, whole_set, value = 1):
+        obs, y = whole_set['observations'], whole_set['y']
+        for i in range(len(obs[:, 0, 0])):
+            if (torch.isnan(y[i])) and (i > 0):
+                y[i] = y [i - 1]
+            for j in range(len(obs[0, :, 0])):
+                for k in range(len(obs[0, 0, :])):
+                    if (torch.isnan(obs[i, j, k])) and (i > 0):
+                        obs[i, j, k] = obs[i - 1, j, k]
+        return {'observations': obs, 'y': y}
 
-    def __call__(self, sample, value = 1):
-        obs, y = sample['Observation'], sample['y']
-        y[y != y] = value
-        return {'Observations': obs, 'y': y}
+class Scaling(object):
+    def __call__(self, whole_set):
+        obs, y = whole_set['observations'], whole_set['y']
+        y_mean = torch.mean(y)
+        y_std = torch.std(y)
+        for i in range(len(obs[:, 0, 0])):
+            y[i] = (y[i] - y_mean)/y_std
+        for i in range(len(obs[0, :, 0])):
+            for j in range(len(obs[0, 0, :])):
+                obs_mean = torch.mean(obs[:, i, j])
+                obs_std = torch.std(obs[:, i, j])
+                for k in range(len(obs[:, 0, 0])):
+                    obs[k, i, j] = (obs[k, i, j] - obs_mean)/obs_std
+        return {'observations': obs, 'y': y}
+
+class Train_Test_split(object):
+
+    def __call__(self, sample):
+        return 0
 
 
 '''TO DO:
